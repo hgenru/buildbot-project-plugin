e882063cd550c17b2dbee8a64fe40d8f2e21bb9a
Alexandr Plesovskih <aplesovskih@naumen.ru>
2013-10-11 16:06:51 +0600

    Its work

--- project.py
+++ project.py
@@ -3,34 +3,37 @@ import imp
 import glob
 
 
-class ProjectFactory(object):
+class Modifier(object):
+    """Wrapper to set project-specify
+       properties to object
+    """
+    def __init__(self, mod_prop):
+        self.mod_prop = mod_prop
+
+    def __set__(self, obj, value):
+        self.value = value
+
+    def __get__(self, obj, objtype):
+        # TODO: Rewrite this dirty hack
+        return [self.mod_obj(v)
+                for v in self.value]
+
+    def mod_object(self, obj):
+        obj.__dict__.update(self.mod_prop)
+        return obj
+
+
+class ProjectFactory(dict):
     """Project Factory
     """
-    class Wrapper(object):
-        """Wrapper to set project-specify
-           properties to object
-        """
-        def __init__(self, mod_prop):
-            self.mod_prop
-
-        def __set__(self, obj, value):
-            self.value = value
-
-        def __get__(self, obj, objtype):
-            return [self.mod_obj(v)
-                    for v in self.value]
-
-        def mod_object(self, obj):
-            obj.__dict__.update(self.mod_prop)
-            return obj
-
     class __metaclass__(type):
         __inheritors__ = []
 
         def __call__(cls, *args, **kwargs):
             obj = type.__call__(cls, *args, **kwargs)
             cls.__inheritors__.append(obj)
-            obj["builders"] = Wrapper
+            obj.__dict__["builders"] = Modifier(
+                {"builddir": os.path.dirname(os.path.realpath(__file__))})
             return obj
 
     def __getattr__(self, attr):
@@ -51,6 +54,7 @@ class ProjectLoader(object):
         self.path = path
         self.file_math = file_math
         self.loaded_projects = self.load_projects_from_path()
+        self.add_projects_to_config()
 
     def load_projects_from_path(self):
         projects_f = glob.iglob(
@@ -58,7 +62,7 @@ class ProjectLoader(object):
         convert = lambda s: "_".join(s.replace(".", "_").split("/")[-2:])
         return [imp.load_source(convert(m), m) for m in projects_f]
 
-    def add_project_to_config(self):
+    def add_projects_to_config(self):
         def add(project):
             to_add = ["slaves",
                       "status",
@@ -67,7 +71,9 @@ class ProjectLoader(object):
                       "change_source"]
             for x in to_add:
                 if project.get(x, None):
-                    self.config[x].append(project[x])
+                    if not (x in self.conf):
+                        self.conf[x] = []
+                    self.conf[x] += project[x]
 
         projects = ProjectFactory.__inheritors__
         for p in projects:
